b793a3033d44a8b2a60ea44b48d7240498f8ed66
Ivan Duran <idurandiaz@gmail.com>
2020-01-25 18:30:35 +0300

    Add sign-in and sign-up page with sign-in component

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInUpPage from './pages/sign-in-up/sign-in-up.component';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/shop" component={ShopPage} />
+      <Route exact path="/signin" component={SignInUpPage} />
     </Switch>
   );
 };
