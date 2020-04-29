d418c6051d185952b3e7cba785bbd1a253c46158
Ivan Duran <idurandiaz@gmail.com>
2020-04-29 04:46:46 +0300

    Save oauth user to state

--- src/App.js
+++ src/App.js
@@ -4,14 +4,37 @@ import './App.css';
 
 import Routes from './routes';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes />
+      </div>
+    );
+  }
 }
 
 export default App;
